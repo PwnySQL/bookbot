bc957a086d6363aa65b333abd72b03a1a0af8db6
Maximilian Lallathin <zenfale@web.de>
2025-11-29 14:45:58 +0100

    ch2l6: count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_chars
 
 
 def get_book_text(filepath: str) -> str:
@@ -11,6 +11,8 @@ def main():
     content = get_book_text('./books/frankenstein.txt')
     num_words = count_words(content)
     print(f"Found {num_words} total words")
+    chars_count = count_chars(content)
+    print(chars_count)
     return 0
 
 
--- stats.py
+++ stats.py
@@ -1,2 +1,9 @@
+from collections import Counter
+
+
 def count_words(text: str) -> int:
     return len(text.split())
+
+
+def count_chars(text: str) -> dict:
+    return Counter(text.lower())
