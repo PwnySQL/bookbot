1cd2f4d3761cb989b7a843055a428ed03326ceb5
Maximilian Lallathin <zenfale@web.de>
2025-11-29 14:28:16 +0100

    ch2l3: read book file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath) -> str:
+    with open(filepath) as f:
+        return f.read()
+    return ''
+
+
+def main():
+    content = get_book_text('./books/frankenstein.txt')
+    print(content)
+
+
+if __name__ == '__main__':
+    main()
