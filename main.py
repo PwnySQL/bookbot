4d4b6edfa12aa62ae4d2ad7f9c242bd7b605aa44
Maximilian Lallathin <zenfale@web.de>
2025-11-29 14:37:22 +0100

    ch2l4: count words

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def get_book_text(filepath) -> str:
+def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         return f.read()
     return ''
 
 
+def count_words(text: str) -> int:
+    return len(text.split())
+
+
 def main():
     content = get_book_text('./books/frankenstein.txt')
-    print(content)
+    num_words = count_words(content)
+    print(f"Found {num_words} total words")
+    return 0
 
 
 if __name__ == '__main__':
